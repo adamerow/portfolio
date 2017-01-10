2b464a5597c52ca72a1e7be02ee8e60e332702eb
adamerow <adamerow@it-mmc16-m12.eastern.ewu.edu>
2017-01-10 14:44:31 -0800

    various
    -fixed sizing/space issue (WOOT!)
    -added orange

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,3 @@
-// ACTIVATE GALLERY
-$('figure').lightGallery();
-    
 $(document).ready(function () {
     var options1 = {
     }
@@ -15,4 +12,36 @@ $(document).ready(function () {
     }
 
     $('.ppy').popeye(options1);
+});
+
+$('.ppy').click(function()
+{
+    //FIND SIZE OF SHOWN IMAGE
+    var width = 0;
+    //get the background-image path
+    var path = $(this).find('.ppy-stage').css('background-image').replace('url', '').replace('(', '').replace(')', '').replace('"', '').replace('"', '');
+    //write HTML for the image
+    var tempImg = '<img id="tempImg" src="' + path + '"/>';
+    // add to DOM before </body>
+    $('body').append(tempImg);
+    //hide image
+    $('#tempImg').hide();
+    //get width
+    width = $('#tempImg').width();
+    //remove from DOM
+    $('#tempImg').remove();
+    
+    //RESIZE PLACEHOLDER TO MATCH IMAGE
+    //if not large
+    if( $(this).hasClass('ppy-expanded'))
+        {
+            $(this).parent().width(262);
+        }
+    
+    //if large
+    else
+    {
+        console.log(width);
+        $(this).parent().width(width);
+    }
 });
